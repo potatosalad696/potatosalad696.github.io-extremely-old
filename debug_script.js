26d2161ee084d7c8bbbfb7965dcf39e8b73cb6f7
potatosalad696 <tjokagungpdp@gmail.com>
2022-02-07 20:29:11 +0800

    debug

--- debug_script.js
+++ debug_script.js
@@ -1,6 +1,6 @@
 function GenerateCode() {
     return "xyxyx-yxyxy-xyxyx-yxyxy-xyxyx".replace(/[xy]/g, function (c) {
-        const r = Math.random() * 16 | 0;
+        const r = Math.random() * 32 | 0;
         const v = c === 'x' ? r : (r & 0x3 | 0x8);
 
         return v.toString(16);
