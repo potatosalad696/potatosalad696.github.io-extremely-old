a563f7472c570f60caae221fe5613d1601c6ce75
potatosalad696 <tjokagungpdp@gmail.com>
2022-02-09 13:49:59 +0800

    debug

--- debug_script.js
+++ debug_script.js
@@ -1,8 +1,14 @@
+function timeouted1() {
+    console.log("TimeoutError: Timeout")
+}
+
 var code = "xxxx-yyyy-xxxx-yyyy".replace(/[xy]/g, function (c) {
     const r = Math.random() * 16 | 0;
     const v = c === 'x' ? r : (r & 0x3 | 0x8);
 
     return v.toString(16);
 });
+
+const timeout1 = setTimeout(timeouted1, 25000)
 
 console.log("The code is: " + code);
