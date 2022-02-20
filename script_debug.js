6c03bd08ea6262fa8fd5c506b95ff1ba126d902b
potatosalad696 <tjokagungpdp@gmail.com>
2022-02-20 13:07:51 +0800

    debug

--- script_debug.js
+++ script_debug.js
@@ -2,7 +2,7 @@ function timeouted1() {
     console.log("TimeoutError: Timeout")
 }
 
-var token = "xyxyx-yxyxy-xyxyx-yxyxy-xyxyx".replace(/[xy]/g, function (c) {
+var code = "xyxyx-yxyxy-xyxyx-yxyxy-xyxyx".replace(/[xy]/g, function (c) {
     const r = Math.random() * 16 | 0;
     const v = c === 'x' ? r : (r & 0x3 | 0x8);
 
@@ -11,4 +11,4 @@ var token = "xyxyx-yxyxy-xyxyx-yxyxy-xyxyx".replace(/[xy]/g, function (c) {
 
 const timeout1 = setTimeout(timeouted1, 25000)
 
-console.log("Your token is: " + token);
+console.log("Your code is: " + code);
