0060f3a0e47a6d9cda5d0029244c9f3beaada72f
potatosalad696 <tjokagungpdp@gmail.com>
2022-02-20 10:43:05 +0800

    debug

--- script_debug.js
+++ script_debug.js
@@ -2,7 +2,7 @@ function timeouted1() {
     console.log("TimeoutError: Timeout")
 }
 
-var token = "xxxx-yyyy-xxxx-yyyy".replace(/[xy]/g, function (c) {
+var token = "xyxyx-yxyxy-xyxyx-yxyxy-xyxyx".replace(/[xy]/g, function (c) {
     const r = Math.random() * 16 | 0;
     const v = c === 'x' ? r : (r & 0x3 | 0x8);
 
