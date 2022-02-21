92dae96fb523378f2149704a7a0d096ed926f0a5
potatosalad696 <tjokagungpdp@gmail.com>
2022-02-21 08:26:53 +0800

    style and debug

--- script_debug.js
+++ script_debug.js
@@ -2,20 +2,11 @@ function timeouted1() {
     console.log("TimeoutError: Timeout")
 }
 
-var code = "yxyxxyxxxyxxxxyxxxxxyxxxx".replace(/[xy]/g, function (c) {
+var code = "xxxxxxxx-xxxx-5xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
     const r = Math.random() * 16 | 0;
     const v = c === 'x' ? r : (r & 0x3 | 0x8);
 
     return v.toString(16);
 });
-
-function check() {
-    const timeout1 = setTimeout(timeouted1, 25000)
-    var input_code = document.getElementById("input").value
-
-    if (input_code == code) {
-        console.log("Correct!")
-    }
-}
 
 console.log("Your code is: " + code);
